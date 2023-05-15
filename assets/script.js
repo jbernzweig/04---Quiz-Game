68a153c7f2beb515d0d6235bbf2ce80264517d7e
Jason Bernzweig <jasonbernzweig@Jasons-Air.lan>
2023-05-15 11:28:23 -0400

    simplified CSS and HTML

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+// function to start the game
+function startGame() {
+
+}
+// function to set a question
+function setNextQuestion(question) {
+
+}
+
+// function to show new question
+function showQuestion(question) {
+
+}
+
+function selectAnswer(e) {
+
+}
+
+const questions = [
+    {
+        question: 'What is 2 + 2?',
+        answers: [
+            { text: 4, correct: true },
+            { text: 22, correct: false},
+        ]
+    }
+]
