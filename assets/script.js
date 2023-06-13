25012dd04d5a73babbdc3b61774d9ad0f7e315b0
Jason Bernzweig <jasonbernzweig@Jasons-Air.lan>
2023-06-13 13:37:09 -0400

    Updates

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,20 @@ function startGame() {
 }
 
 // timer 
+const startingMinutes = 1;
+let time = startingMinutes * 60;
 
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = `${seconds}`;
+    time--;
+}
 
 
 
